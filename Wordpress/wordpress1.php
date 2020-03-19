1bcef7f896a22eaf30749e05375880ba6fcfe92b
arianaandersoncodebank <61384482+arianaandersoncodebank@users.noreply.github.com>
2020-03-19 08:57:01 +0530

    Update wordpress1.php

--- Wordpress/wordpress1.php
+++ Wordpress/wordpress1.php
@@ -1,2 +1,6 @@
 1) How To Use jQuery Ajax In WordPress
 https://www.youtube.com/watch?v=APaCwk77hCc
+
+
+2)  Wordpress queries
+https://codex.wordpress.org/Class_Reference/wpdb#Running_General_Queries
