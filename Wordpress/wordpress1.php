b24d15c2ac485086dd4aa8fe40aa8346cf55c866
arianaandersoncodebank <61384482+arianaandersoncodebank@users.noreply.github.com>
2020-03-19 09:05:29 +0530

    Update wordpress1.php

--- Wordpress/wordpress1.php
+++ Wordpress/wordpress1.php
@@ -4,3 +4,25 @@ https://www.youtube.com/watch?v=APaCwk77hCc
 
 2)  Wordpress queries
 https://codex.wordpress.org/Class_Reference/wpdb#Running_General_Queries
+
+3) google= get current page slug name in wordpress
+
+<?php 
+    global $post;
+    $post_slug = $post->post_name;
+?>
+
+or
+
+$slug = get_post_field( 'post_name');
+
+
+4) google == wordpress get current page id
+
+global $post;
+echo $post->ID;
+
+OR
+
+global $wp_query;
+echo $wp_query->post->ID;
